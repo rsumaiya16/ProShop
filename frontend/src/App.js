ba3b5f001921d915a13ad790021c2e00e76e784d
rsumaiya16 <rsumaiya1602@gmail.com>
2025-03-02 08:50:16 +0600

    test4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+// src/App.js
+import React from 'react';
+import { Container } from 'react-bootstrap'; // Import Container from react-bootstrap
+import Header from './components/Header'; // Correct import of Header
+import Footer from './components/Footer'; // Correct import of Footer
+
 
 const App = () => {
+  
   return (
-    <h1>welcome to proshop</h1>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1> {/* Inside the Container for proper layout */}
+        </Container>
+      </main>
+     
+ 
+    </>
   );
-}
+};
 
-export default App
+export default App;
